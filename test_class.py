577fb12a72a7c3d76a21265ca0ed8a83eee942da
hyex <48546343+hyex@users.noreply.github.com>
2019-07-26 14:31:05 -0400

    Update test_class.py

--- test_class.py
+++ test_class.py
@@ -1,3 +1,20 @@
 from esc_class import bldc_motor
 
 var = bldc_motor(4, 2000, 700, 800)
+print "For first time launch, select calibrate"
+print "Type the exact word for the function you want"
+print "calibrate OR manual OR control OR arm OR stop"
+
+inp = raw_input()
+if inp == "manual":
+    var.manual_drive()
+elif inp == "calibrate":
+    var.calibrate()
+elif inp == "arm":
+    var.arm()
+elif inp == "control":
+    var.control()
+elif inp == "stop":
+    var.stop()
+else :
+    print "Thank You for not following the things I'm saying... now you gotta restart the program STUPID!!"
