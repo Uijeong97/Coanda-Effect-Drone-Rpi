bea79dafb5301d37024813a4700280562411b431
hyex <sosox00@naver.com>
2019-08-11 16:08:18 -0400

    test gyro

--- test_gyro.py
+++ test_gyro.py
@@ -1,238 +1,65 @@
-import smbus
-import math
-import time
-
+import Gyro as Gyro
  
-# Power management registers
-
-
-power_mgmt_1 = 0x6b
-power_mgmt_2 = 0x6c
-
-def read_byte(adr):
-    return bus.read_byte_data(address, adr)
-
-def read_word(adr):
-    high = bus.read_byte_data(address, adr)
-    low = bus.read_byte_data(address, adr+1)
-    val = (high << 8) + low
-    return val 
-
-def read_word_2c(adr):
-
+gyro = Gyro()
  
+def calc_average(gyro_xout, gyro_yout, gyro_zout, accel_xout, accel_yout, accel_zout):
+   
+    num_readings = 10;
 
-    val = read_word(adr)
+    print "Starting Calibration"
 
- 
+    for i in range (num_readings):
+        gyro_xout, gyro_yout, gyro_zout += gyro.get_gyro_data_lsb()
+        accel_xout, accel_yout, accel_zout += gyro.get_accel_data_lsb()
+        delay(100)
 
-    if (val >= 0x8000):
+    accel_xout /= num_readings
+    accel_yout /= num_readings
+    accel_zout /= num_readings
+    gyro_xout /= num_readings
+    gyro_yout /= num_readings
+    gyro_zout /= num_readings
 
- 
+    # Store the raw calibration values globally
 
-        return -((65535 - val) + 1)
+    base_x_accel = accel_xout
+    base_y_accel = accel_yout 
+    base_z_accel = accel_zout 
+    base_x_gyro = gyro_xout
+    base_y_gyro = gyro_yout
+    base_z_gyro = gyro_zout
 
- 
+    print "Finishing Calibration"
 
-    else:
+    return [base_x_gyro, base_y_gyro, base_z_gyro, base_x_accel, base_y_accel, base_z_accel]
 
- 
-
-        return val
-
- 
-
- 
-
- 
-
-def dist(a,b):
-
- 
-
-    return math.sqrt((a*a)+(b*b))
-
- 
-
- 
-
- 
-
-def get_y_rotation(x,y,z):
-
- 
-
-    radians = math.atan2(x, dist(y,z))
-
- 
-
-    return -math.degrees(radians)
-
- 
-
- 
-
- 
-
-def get_x_rotation(x,y,z):
-
- 
-
-    radians = math.atan2(y, dist(x,z))
-
- 
-
-    return math.degrees(radians)
-
- 
-
- 
-
- 
-
-bus = smbus.SMBus(1) # or bus = smbus.SMBus(1) for Revision 2 boards
-
- 
-
-address = 0x68       # This is the address value read via the i2cdetect command
-
- 
-
- 
-
- 
-
-# Now wake the 6050 up as it starts in sleep mode
-
- 
-
-bus.write_byte_data(address, power_mgmt_1, 0)
-
- 
-
- 
 
 try:
-
     while True:
-
- 
-
- 
-
                 time.sleep(1)
-
- 
-
                 print "--------------------------------------------------------"
-
- 
-
-                print "=>gyro data"
-
- 
-
+                print "=>init gyro data"
                 print "---------"
-
- 
-
-                gyro_xout = read_word_2c(0x43)
-
- 
-
-                gyro_yout = read_word_2c(0x45)
-
- 
-
-                gyro_zout = read_word_2c(0x47)
-
- 
-
-                print "gyro_xout: ", gyro_xout, " scaled: ", (gyro_xout / 131)
-
- 
-
-                print "gyro_yout: ", gyro_yout, " scaled: ", (gyro_yout / 131)
-
- 
-
-                print "gyro_zout: ", gyro_zout, " scaled: ", (gyro_zout / 131)
-
- 
-
- 
-
- 
-
+                gyro_xout, gyro_yout, gyro_zout = gyro.get_gyro_data_lsb()
+                print "gyro_xout: ", gyro_xout
+                print "gyro_yout: ", gyro_yout
+                print "gyro_zout: ", gyro_zout
                 print
-
- 
-
-                print "=>accelerometer data"
-
- 
-
+                print "=> init accelerometer data"
                 print "------------------"
-
- 
-
- 
-
- 
-
-                accel_xout = read_word_2c(0x3b)
-
- 
-
-                accel_yout = read_word_2c(0x3d)
-
- 
-
-                accel_zout = read_word_2c(0x3f)
-
- 
-
- 
-
- 
-
-                accel_xout_scaled = accel_xout / 16384.0
-
- 
-
-                accel_yout_scaled = accel_yout / 16384.0
-                accel_zout_scaled = accel_zout / 16384.0
-
- 
-
-                print "accel_xout: ", accel_xout, " scaled: ", accel_xout_scaled
-
- 
-
-                print "accel_yout: ", accel_yout, " scaled: ", accel_yout_scaled
-
- 
-
-                print "accel_zout: ", accel_zout, " scaled: ", accel_zout_scaled
-
- 
-
-                print "x rotation: " , get_x_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
-
- 
-
-                print "y rotation: " , get_y_rotation(accel_xout_scaled, accel_yout_scaled, accel_zout_scaled)
-
- 
-
- 
-
+                accel_xout, accel_yout, accel_zout = gyro.get_accel_data_lsb()
+                print "accel_xout: ", accel_xout, 
+                print "accel_yout: ", accel_yout, 
+                print "accel_zout: ", accel_zout, 
+                print
+                print "Confirm accel_zout is near by 16384 --> then OK" 
                 print "--------------------------------------------------------"
-
- 
-
- 
+                print
+                print "보정 값"
+                print "--------------------------------------------------------"
 
 except KeyboardInterrupt:
 
      GPIO.cleanup()
+
+ 
